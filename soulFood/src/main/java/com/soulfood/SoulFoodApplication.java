541ac77192e3059b9f87de4e9b4818b8a837690c
vinitshahilverma <vinitshahilverma@gmail.com>
2022-11-09 10:00:09 +0530

    packages added

---
+++ soulFood/src/main/java/com/soulfood/SoulFoodApplication.java
@@ -0,0 +1,13 @@
+package com.soulfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulFoodApplication.class, args);
+	}
+
+}
---
+++ soulFood/src/test/java/com/soulfood/SoulFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
