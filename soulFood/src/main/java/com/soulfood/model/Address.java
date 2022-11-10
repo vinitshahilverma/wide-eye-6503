f100becfffead8872ac5ad3e1c9482a0f4cd10b4
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-11-10 20:40:13 +0530

    unused import statements removed from the model classes

--- soulFood/src/main/java/com/soulfood/model/Address.java
+++ soulFood/src/main/java/com/soulfood/model/Address.java
@@ -1,6 +1,5 @@
 package com.soulfood.model;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
