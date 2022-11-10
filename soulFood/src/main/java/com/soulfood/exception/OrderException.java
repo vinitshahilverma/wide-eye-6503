c6f3c34b403576cd2e8229870cb954d91459314f
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-11-10 13:11:54 +0530

    Order Exception class, Order Service class and implementation class added

---
+++ soulFood/src/main/java/com/soulfood/exception/OrderException.java
@@ -0,0 +1,13 @@
+package com.soulfood.exception;
+
+public class OrderException extends Exception {
+    public OrderException(){
+
+    }
+
+    public OrderException(String message) {
+        super(message);
+    }
+
+    
+}
--- soulFood/src/main/java/com/soulfood/service/OrderService.java
+++ soulFood/src/main/java/com/soulfood/service/OrderService.java
@@ -1,5 +1,18 @@
 package com.soulfood.service;
 
+import com.soulfood.exception.OrderException;
+import com.soulfood.model.Order;
+
 public interface OrderService {
     
+    public Order addOrder(Order order) throws OrderException;
+
+    public Order updateOrder(Order order) throws OrderException;
+
+    public Order removeOrder(Integer orderId) throws OrderException;
+
+    public Order viewOrder(Integer orderId) throws OrderException;
+
+    
+
 }
